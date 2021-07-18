e9f18cdb932df58dc0f3caf1ec5a0f7e0758cf0f
bpa03 <baldassarepugliese@gmail.com>
2021-07-18 15:25:41 -0400

    Firebase

--- src/firebase/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyBixw52AA6WPR1sL-WEuYbuSH3tfUZN4us",
-  authDomain: "crwn-db-3f909.firebaseapp.com",
-  projectId: "crwn-db-3f909",
-  storageBucket: "crwn-db-3f909.appspot.com",
-  messagingSenderId: "960991625282",
-  appId: "1:960991625282:web:65dc0c33342e7e10c3eddb",
-  measurementId: "G-H4WSNKP50C"
-};
